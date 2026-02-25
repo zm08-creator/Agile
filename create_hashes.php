33f1903524e3cfe594fc4cdf0b9fd48ea8aef65f
Zainub <znb_mth@yahoo.com>
2026-02-25 22:11:40 +0000

    deleted hash file so hashes cannot be copied by anyone

--- create_hashes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "patient: " . password_hash("patient", PASSWORD_DEFAULT) . "<br>";
-echo "professional: " . password_hash("professional", PASSWORD_DEFAULT) . "<br>";
-echo "admin: " . password_hash("admin", PASSWORD_DEFAULT) . "<br>";
-
-?>
