a233743fdff21b20263993405538fcb071d17ca8
svoraji <savoraji@uclan.ac.uk>
2026-03-10 00:12:30 +0000

    Update ProfDash.php

--- ProfDash.php
+++ ProfDash.php
@@ -2,10 +2,13 @@
 session_start();
 
 // MUST be logged in as Professional (user_id = 2)
-if (!isset($_SESSION["user_id"]) || $_SESSION["user_id"] != 2) {
+if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "professional") 
+{
     header("Location: Login.php");
     exit;
 }
+
+
 ?>
 
 <!DOCTYPE html>
