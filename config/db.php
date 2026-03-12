58fa286984b37d94590efc535bce1dcd528d1c6f
Zainub <znb_mth@yahoo.com>
2026-03-12 12:31:09 +0000

    db changes

--- config/db.php
+++ config/db.php
@@ -1,14 +1,20 @@
 <?php
+// PostgreSQL connection for agile_db
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "bookings";
+$host     = "localhost";
+$port     = "5432";          // Default PostgreSQL port
+$dbname   = "agile_db";
+$user     = "postgres";      // You said this username is correct
+$password = "REPLACE_WITH_YOUR_PASSWORD";
 
-$conn = new mysqli($host, $username, $password, $database);
+// Build connection string
+$connStr = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Open connection
+$conn = pg_connect($connStr);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
 }
-
 ?>
