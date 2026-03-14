473ac96670244ac2d12e3cb00584f2e6d5f4356a
Zainub <znb_mth@yahoo.com>
2026-03-14 16:28:15 +0000

    db changes so doctors have a user id

--- config/agile.sql
+++ config/agile.sql
@@ -107,6 +107,10 @@ ALTER TABLE `Patient`
     ADD UNIQUE KEY `user_id` (`user_id`),
     ADD CONSTRAINT `patient_ibfk_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
     ALTER TABLE `Bookings` ADD COLUMN `Discussion` varchar(255) NOT NULL;
+    ALTER TABLE `Doctor`
+    ADD COLUMN `user_id` int(11) DEFAULT NULL,
+    ADD UNIQUE KEY `doctor_user_id` (`user_id`),
+    ADD CONSTRAINT `doctor_ibfk_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
